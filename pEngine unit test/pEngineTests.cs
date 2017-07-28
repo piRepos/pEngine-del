a19596d3f715b57ccfc4579a73d07fdb0331a807
AndreaDemontis <pk.demontis@gmail.com>
2017-07-28 22:54:50 +0200

    Unit test fix.

---
+++ pEngine unit test/pEngineTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace pEngineTests
+{
+	[TestClass]
+	public class pEngineTests
+	{
+		[TestMethod]
+		public void ImplementTests()
+		{
+			// TODO: Implement tests
+			Assert.IsTrue(true);
+		}
+	}
+}
